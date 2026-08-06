373dfb7cb7210f0038c784f66a282fff5c389a9e
Mozza736 <dave@roevisual.eu>
2026-08-06 16:35:32 +0100

    Split Made Current homepage into crawlable sections

--- home.php
+++ home.php
@@ -1,2 +1,2 @@
 <?php
-foreach (['01','02','03','04'] as $part) { include __DIR__ . '/partials/' . $part . '.html'; }
+foreach (["01", "work", "problem", "method", "fit", "pricing", "about", "faq", "contact", "footer"] as $part) { include __DIR__ . '/partials/' . $part . '.html'; }
